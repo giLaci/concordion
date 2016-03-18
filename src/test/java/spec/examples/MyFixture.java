d06b51e93d249b2619910f25db56aa271927169c
Laci <Laci@ls-PC>
2016-03-18 15:34:41 +0100

    Fixing - Add imports for MyFixture.java

--- src/test/java/spec/examples/MyFixture.java
+++ src/test/java/spec/examples/MyFixture.java
@@ -1,4 +1,10 @@
+package spec.examples;
 
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.concordion.internal.util.SimpleFormatter;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
 public class MyFixture {
 
 }
